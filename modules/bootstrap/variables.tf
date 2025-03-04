e55685010ef6ef5384b779d4178f08728d65da16
lukcic <lukcic@int.pl>
2025-03-04 22:54:49 +0100

    Variables validation

--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -5,17 +5,26 @@ variable "project_name" {
 
 variable "subject_area" {
   type        = string
-  description = "Name of subject area"
+  description = "Name of the subject area"
+  validation {
+    condition     = can(regex("^(01|02|03)$", var.subject_area))
+    error_message = "Must be: 01/02/03"
+  }
 }
 
 variable "environment" {
   type        = string
   description = "Name of environment"
+  validation {
+    condition     = can(regex("^(dev|qa|prod)$", var.environment))
+    error_message = "Must be: dev/qa/prod"
+  }
 }
 
 variable "location" {
   type        = string
   description = "Location of the resources"
+  default     = "West Europe"
 }
 
 variable "subscription_id" {
